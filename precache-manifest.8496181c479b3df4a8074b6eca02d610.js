31d6b446f0a20260c996d13432f556458c2e4f46
unknown <chocolatyanuragh@gmail.com>
2020-06-23 16:59:16 +0530

    Updates

---
+++ precache-manifest.8496181c479b3df4a8074b6eca02d610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fddde5412606d70abfa",
+    "url": "/LiveStock/static/css/main.9b2f9bd2.chunk.css"
+  },
+  {
+    "revision": "0fddde5412606d70abfa",
+    "url": "/LiveStock/static/js/main.0fddde54.chunk.js"
+  },
+  {
+    "revision": "67a1741c1a3330eb221f",
+    "url": "/LiveStock/static/css/1.6b94ced3.chunk.css"
+  },
+  {
+    "revision": "67a1741c1a3330eb221f",
+    "url": "/LiveStock/static/js/1.67a1741c.chunk.js"
+  },
+  {
+    "revision": "70825332922340e24b7a",
+    "url": "/LiveStock/static/js/runtime~main.70825332.js"
+  },
+  {
+    "revision": "a11f88abe92df4c83c67209b44e6dc5b",
+    "url": "/LiveStock/index.html"
+  }
+];
